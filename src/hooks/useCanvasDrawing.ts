9194fe8776c6ef4f6c7ea018d920eb466bc6a98a
dliutyi <ljutyjj.dima@gmail.com>
2025-11-23 11:43:12 +0000

    fix: remove unused frameToParent import

--- src/hooks/useCanvasDrawing.ts
+++ src/hooks/useCanvasDrawing.ts
@@ -1,7 +1,7 @@
 import { useState, useRef, useCallback } from 'react'
 import type { ViewportState, CoordinateFrame, Point2D, FrameBounds } from '../types'
 import { screenToWorld, snapPointToGrid, worldToScreen } from '../utils/coordinates'
-import { screenToFrame, parentToFrame, nestedFrameToScreen, frameToParent, frameCoordsToParentWorld } from '../utils/frameTransforms'
+import { screenToFrame, parentToFrame, nestedFrameToScreen, frameCoordsToParentWorld } from '../utils/frameTransforms'
 
 interface UseCanvasDrawingProps {
   isDrawing: boolean
